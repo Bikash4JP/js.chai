fa0f0d333c50b253a144ed764107965b11153361
Bikash4JP <152150523+Bikash4JP@users.noreply.github.com>
2023-11-27 08:22:30 +0000

    basic1

--- jsBasic/test.js
+++ jsBasic/test.js
@@ -1,1 +1,19 @@
-console.log("BIkash");
+// console.log("BIkash");
+const userId = "230039"
+let userMail = " abc.com"
+var userPassword = "Biu87"
+userCity="Yokohama"
+let userState;
+
+// userId="4" value of const can not be change
+userMail="BIkash4JP.com"
+userPassword= "09876"
+userCity="Nepal"
+console.table([userId,userMail,userPassword,userCity,userState])
+
+// var is replaced by let so dont use var , because of issue og local and global scope
+
+
+
+
+// console.log(userId);
